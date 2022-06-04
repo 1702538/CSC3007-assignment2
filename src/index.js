a95e4563a6a2f5e15572e18aa957b03ff77334f3
Keith Chong <70758815+1702538@users.noreply.github.com>
2022-06-04 21:25:02 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,20 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import CrimeByType from "./CrimeTypeComponent";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<App />}></Route>
-        <Route
-          path="/CSC3007-assignment2/crimetype"
-          element={<CrimeByType />}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+    <HashRouter>
+      <Route exact path="/" component={App} />
+      <Route exact path="/posts" component={CrimeByType} />
+    </HashRouter>
   </React.StrictMode>
 );
 
@@ -24,3 +19,10 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// <BrowserRouter basename={process.env.PUBLIC_URL}>
+//   <Routes>
+//     <Route path="/" component={CrimeByType}></Route>
+//     <Route path="/crimetype" component={CrimeByType}></Route>
+//   </Routes>
+// </BrowserRouter>;
