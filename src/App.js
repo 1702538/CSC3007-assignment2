47af6f4e979c6f902c7112d56f1ca1eef68cb048
Keith Chong <70758815+1702538@users.noreply.github.com>
2022-06-07 14:02:46 +0800

    Add BrowserRouter code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import "./App.css";
 import "antd/dist/antd.css";
 import CrimeChart from "./CrimeChartComponent";
+import CrimeType from "./CrimeTypeComponent";
+
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <CrimeChart />
+      <h1>123</h1>
+      <Router basename="/CSC3007-assignment2">
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/crimetype">Crime Type</Link>
+            </li>
+          </ul>
+
+          <hr />
+
+          <Route exact path="/" component={CrimeChart} />
+          <Route path="/crimetype" component={CrimeType} />
+        </div>
+      </Router>
     </div>
   );
 }
